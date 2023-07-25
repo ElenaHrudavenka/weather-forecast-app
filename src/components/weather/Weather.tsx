9fea2f62c6f60b72ca0e9ee3d17da8be584d1568
ElenaHrudavenka <kisseli@mail.ru>
2023-07-25 19:51:00 +0300

    Layout and requests were added.

---
+++ src/components/weather/Weather.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Weather.module.scss'
+const Weather = () => {
+    return (
+        <div className={s.wrapper}>
+
+        </div>
+    );
+};
+
+export default Weather;
