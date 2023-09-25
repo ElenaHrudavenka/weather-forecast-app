135b4a4e32cf1931121b6144f6aea24e60512d5d
ElenaHrudavenka <kisseli@mail.ru>
2023-09-26 00:11:55 +0200

    Middleware cors.ts was changed.

--- src/services/cors.ts
+++ src/services/cors.ts
@@ -16,6 +16,8 @@ export async function handleRequest(request: Request): Promise<Response> {
       body: request.body,
     });
     response = new Response(response.body, response);
+    console.log('body:');
+    console.dir(response.json().then((res) => console.log(res)));
     response.headers.set('Access-Control-Allow-Origin', 'https://elenahrudavenka.github.io');
     return response;
   } catch (err: any) {
