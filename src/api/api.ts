a7806c8c5b889dba4acc8ad27c8109b749cf3adc
ElenaHrudavenka <kisseli@mail.ru>
2023-09-26 17:40:55 +0200

    Loader

--- src/api/api.ts
+++ src/api/api.ts
@@ -8,12 +8,6 @@ const username = 'hrudavenka_hrudavenka';
 const password = '1n8Oj6TZ5i';
 let headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
-headers.set('Host', 'elenahrudavenka.github.io');
-/*const requestToken = new Request('https://login.meteomatics.com/api/v1/token', {
-  method: 'GET',
-  credentials: 'include',
-  headers: headers,
-});*/
 
 export const AppAPI = {
   // Получаю токен для авторизации на meteomatics.com
